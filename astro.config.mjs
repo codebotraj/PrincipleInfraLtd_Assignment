0b26f7383091b06ec7b6a248ed74b90a70316c13
codebotraj <rajvardhanupatil4152@gmail.com>
2023-08-10 22:55:56 +0530

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://codebotraj.github.io',
+  base: '/PrincipleInfraLtd_Assignment',
   integrations: [tailwind()]
 });
